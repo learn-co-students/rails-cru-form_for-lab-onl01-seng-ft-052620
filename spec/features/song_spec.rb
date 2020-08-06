0a0ac1c589481f78e59da5fc20ef53b2ab9a41ca
Kristin Howard <hkristin@andrew.cmu.edu>
2020-08-06 01:18:20 +0000

    Done.

--- spec/features/song_spec.rb
+++ spec/features/song_spec.rb
@@ -8,17 +8,18 @@ describe 'navigate song pages' do
   end
 
   it 'shows the name on the show page in a h1 tag' do
+    #binding.pry
     visit song_path(@song.id)
     expect(page).to have_css("h1", text: "My Song")
   end
 
-  it 'displays a link to the genre page' do 
+  it 'displays a link to the genre page' do
     visit song_path(@song.id)
 
     expect(page).to have_link(@genre.name, href: genre_path(@genre))
   end
 
-  it 'displays a link to the artist page' do 
+  it 'displays a link to the artist page' do
     visit song_path(@song.id)
 
     expect(page).to have_link(@artist.name, href: artist_path(@artist))
@@ -75,5 +76,5 @@ describe 'song index' do
     expect(page).to have_css("p", text: "My Song")
   end
 
-  
+
 end
